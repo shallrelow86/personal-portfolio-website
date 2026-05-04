817fc353b470edd28b39fbf99179b06017f3674d
shallrelow <2475458971@qq.com>
2026-05-04 12:06:57 +0800

    chore: scaffold Next.js project with Sanity and dependencies

---
+++ sanity.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './sanity/schemas';
+
+export default defineConfig({
+  name: 'portfolio',
+  title: 'Personal Portfolio',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  plugins: [structureTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+});
